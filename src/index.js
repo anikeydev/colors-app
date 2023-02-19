98fbdb9459653ec04028c39ecebbcc1d7233acbc
anikeydev <dev.anikey.va@mail.ru>
2023-02-19 19:15:19 +0300

    refactoring handlers functions

--- src/index.js
+++ src/index.js
@@ -8,59 +8,76 @@ import createNotification from './view/notification.js'
 const $app = document.querySelector('#app')
 const $modal = createModal('This is color picker app', 'Press "Space" to refresh colors and press "lock" to don`t refresh color')
 
-document.addEventListener('DOMContentLoaded', $app.appendChild($modal))
- 
-document.addEventListener('keypress', (event) => {
-    if (toLowerCase(event.code) === 'space') {
-        if (document.querySelector('.app__content')) {
-            generateColors()
-        } 
-    }
+renderModal()
 
-    if(toLowerCase(event.code) === 'enter') {
-        if (document.querySelector('.modal')) {
-            createHtmlApp()
-        }
-    }
-})
+function renderModal() {
+    $app.appendChild($modal)
 
-$app.addEventListener('click', handlerClickStartBtn)
-
-$app.addEventListener('click', (event) => {
-    if (event.target.dataset.lock) {
-        event.target.dataset.lock = event.target.dataset.lock === 'false' ? 'true' : 'false' 
-        event.target.classList.toggle('fa-lock-open')
-        event.target.classList.toggle('fa-lock')
-        console.log(event.target)
-    }    
-
-    if (event.target.dataset.type === 'help-btn') {
-        $app.innerHTML = ''
-        $app.appendChild($modal)
-    }
-
-    if (event.target.dataset.type === 'hash') {
-        const hash = event.target.innerText
-        navigator.clipboard.writeText(hash)
-        createNotification($app,'hash copy', { hash, delay: '5s'})
-    }
-})
+    document.addEventListener('keypress', handlerPressEnter)
+    
+    $app.addEventListener('click', handlerClickStartBtn)
+}
 
 function handlerClickStartBtn(event) {
     if (event.target.dataset.type === 'btn') {
         const $input = document.querySelector('.modal__input')
         if ($input.value) {
-            createHtmlApp()
+            createApp()
         } else {
             createNotification($app, 'Min count', { hash: 1 })
         }
     }
 }
 
-function createHtmlApp() {
+function handlerClickHelpBtn(event) {
+    if (event.target.dataset.type === 'help-btn') {
+        $app.innerHTML = ''
+        renderModal()
+    }
+}
+
+function handlerClickToLock(event) {
+    if (event.target.dataset.lock) {
+        event.target.dataset.lock = event.target.dataset.lock === 'false' ? 'true' : 'false' 
+        event.target.classList.toggle('fa-lock-open')
+        event.target.classList.toggle('fa-lock')
+    } 
+}
+
+function handlerClickToHash(event) {
+    if (event.target.dataset.type === 'hash') {
+        const hash = event.target.innerText
+        navigator.clipboard.writeText(hash)
+        createNotification($app,'hash copy', { hash, delay: '5s'})
+    }
+}
+
+function handlerPressSpace(event) {
+    if (toLowerCase(event.code) === 'space') {
+        if (document.querySelector('.app__content')) {
+            generateColors()
+        } 
+    }
+}
+
+function handlerPressEnter(event) {
+    if(toLowerCase(event.code) === 'enter') {
+        if (document.querySelector('.modal')) {
+            createApp()
+        }
+    }
+}
+
+function createApp() {
     const $input = document.querySelector('.modal__input')
     $app.innerHTML = ''
+    $app.removeEventListener('click', handlerClickStartBtn)
+    $app.removeEventListener('keypress', handlerPressEnter)
     $app.appendChild(createContent($input.value))
+    document.addEventListener('keypress', handlerPressSpace)
+    $app.addEventListener('click', handlerClickHelpBtn)
+    $app.addEventListener('click', handlerClickToLock)
+    $app.addEventListener('click', handlerClickToHash) 
 }
 
 function generateColors() {
